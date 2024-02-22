0c0281bd512f3c99cb2359d3ae9f30c66a74808f
Mirelle Alessandre <mirelle.alessandre@arctouch.com>
2024-02-22 09:36:28 -0300

    Change pokemon profile
    Category and ability

--- Pokedex/PokemonProfileViewController.swift
+++ Pokedex/PokemonProfileViewController.swift
@@ -89,22 +89,20 @@ class PokemonProfileViewController: UIViewController {
         let feature = PokemonFeature()
         feature.translatesAutoresizingMaskIntoConstraints = false
         feature.title.text = "Category"
-        feature.data.text = "Seed"
         return feature
     }()
     
-    private lazy var pokemonHability: PokemonFeature = {
+    private lazy var pokemonAbility: PokemonFeature = {
         let feature = PokemonFeature()
         feature.translatesAutoresizingMaskIntoConstraints = false
-        feature.title.text = "Hability"
-        feature.data.text = "Overgrow"
+        feature.title.text = "Ability"
         return feature
     }()
 
     private lazy var secondFeatureStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.addArrangedSubviews([pokemonCategory, pokemonHability])
+        stackView.addArrangedSubviews([pokemonCategory, pokemonAbility])
         stackView.axis = .horizontal
         stackView.spacing = 20
         stackView.distribution = .fillEqually
@@ -167,7 +165,8 @@ class PokemonProfileViewController: UIViewController {
         pokemonDescription.text = pokemon.description
         pokemonWeight.data.text = pokemon.profile.weight
         pokemonHeight.data.text = pokemon.profile.height
-
+        pokemonCategory.data.text = pokemon.species
+        pokemonAbility.data.text = pokemon.profile.ability[0][0]
     }
     
     private func configureUI() {
@@ -207,77 +206,3 @@ class PokemonProfileViewController: UIViewController {
         ])
     }
 }
-
-final class PokemonWeaknessView: UIView {
-    lazy var firstView: TypeView2 = {
-        let view = TypeView2()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    lazy var secondView: TypeView2 = {
-        let view = TypeView2()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-
-    private lazy var primaryStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.addArrangedSubviews([firstView, secondView])
-        stackView.axis = .horizontal
-        stackView.spacing = 20
-        stackView.distribution = .fillEqually
-        return stackView
-    }()
-    
-    lazy var thirdView: TypeView2 = {
-        let view = TypeView2()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    lazy var fourthView: TypeView2 = {
-        let view = TypeView2()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-
-    private lazy var secondaryStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.addArrangedSubviews([thirdView, fourthView])
-        stackView.axis = .horizontal
-        stackView.spacing = 20
-        stackView.distribution = .fillEqually
-        return stackView
-    }()
-    
-    init() {
-        super.init(frame: .zero)
-        addSubviews([firstView, secondView, thirdView, fourthView])
-        
-        NSLayoutConstraint.activate([
-            firstView.topAnchor.constraint(equalTo: topAnchor, constant: 10),
-            firstView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            
-            secondView.topAnchor.constraint(equalTo: topAnchor, constant: 10),
-            secondView.leadingAnchor.constraint(equalTo: firstView.trailingAnchor, constant: 10),
-            secondView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
-            
-            thirdView.topAnchor.constraint(equalTo: firstView.bottomAnchor, constant: 10),
-            thirdView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            thirdView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
-            
-            fourthView.topAnchor.constraint(equalTo: secondView.bottomAnchor, constant: 10),
-            fourthView.leadingAnchor.constraint(equalTo: thirdView.trailingAnchor, constant: 10),
-            fourthView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
-            fourthView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10)
-        ])
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
--- Pokedex/PokemonWeaknessView.swift
+++ Pokedex/PokemonWeaknessView.swift
@@ -1,8 +1,76 @@
-//
-//  PokemonWeaknessView.swift
-//  Pokedex
-//
-//  Created by Mirelle Alessandre on 21/02/24.
-//
+import UIKit
 
-import Foundation
+final class PokemonWeaknessView: UIView {
+    private lazy var firstView: TypeView = {
+        let view = TypeView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var secondView: TypeView = {
+        let view = TypeView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private lazy var primaryStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.addArrangedSubviews([firstView, secondView])
+        stackView.axis = .horizontal
+        stackView.spacing = 20
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    private lazy var thirdView: TypeView = {
+        let view = TypeView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var fourthView: TypeView = {
+        let view = TypeView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private lazy var secondaryStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.addArrangedSubviews([thirdView, fourthView])
+        stackView.axis = .horizontal
+        stackView.spacing = 20
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        addSubviews([firstView, secondView, thirdView, fourthView])
+        
+        NSLayoutConstraint.activate([
+            firstView.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            firstView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            
+            secondView.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            secondView.leadingAnchor.constraint(equalTo: firstView.trailingAnchor, constant: 10),
+            secondView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            
+            thirdView.topAnchor.constraint(equalTo: firstView.bottomAnchor, constant: 10),
+            thirdView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            thirdView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
+            
+            fourthView.topAnchor.constraint(equalTo: secondView.bottomAnchor, constant: 10),
+            fourthView.leadingAnchor.constraint(equalTo: thirdView.trailingAnchor, constant: 10),
+            fourthView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            fourthView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10)
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureView(with )
+}
