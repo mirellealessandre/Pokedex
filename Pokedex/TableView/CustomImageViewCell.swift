7fbac09d95c2e1cd1c347125cdc7cd5ac34d9071
Mirelle Alessandre <mirelle.alessandre@arctouch.com>
2024-03-02 11:36:38 -0300

    Create CustomImageViewCell
    Load image cell from URL

---
+++ Pokedex/TableView/CustomImageViewCell.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+let imageCache = NSCache<AnyObject, AnyObject>()
+
+final class CustomImageViewCell: UIImageView {
+    var task: URLSessionDataTask!
+    let spinner = UIActivityIndicatorView(style: .medium)
+    
+    func loadImageView(from url: URL) {
+        image = nil
+        
+        addSpinner()
+        
+        if let task = task {
+            task.cancel()
+        }
+        
+        if let imageFromCache = imageCache.object(forKey: url.absoluteString as AnyObject) as? UIImage {
+            image = imageFromCache
+            removeSpinner()
+            return
+        }
+        
+        task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data, let newImage = UIImage(data: data) else {
+                print("Couldn't load image from URL: \(url)")
+                return
+            }
+            
+            imageCache.setObject(newImage, forKey: url.absoluteString as AnyObject)
+            
+            DispatchQueue.main.async {
+                self.image = newImage
+                self.removeSpinner()
+            }
+        }
+        task.resume()
+    }
+    
+    func addSpinner() {
+        addSubview(spinner)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+        
+        spinner.startAnimating()
+    }
+    
+    func removeSpinner() {
+        spinner.removeFromSuperview()
+    }
+}
